d1d2120c868f0ae5c19eda2e1428adf7b43b307a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-06-26 18:29:47 -0400

    Setted up basic project structure.

---
+++ src/main.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = __importDefault(require("discord.js"));
+const secrets_json_1 = __importDefault(require("../data/secrets.json"));
+let client = new discord_js_1.default.Client({
+    intents: [
+        discord_js_1.default.Intents.FLAGS.GUILDS,
+        discord_js_1.default.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+client.once("ready", (client) => {
+    console.log(`[INFO]: Logged in to Discord as ${client.user.tag}.`);
+});
+client.login(secrets_json_1.default.TOKEN);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import Discord from "discord.js"
+import Secrets from "../data/secrets.json"
+
+let client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.once("ready", (client) => {
+    console.log(`[INFO]: Logged in to Discord as ${client.user.tag}.`)
+})
+
+client.login(Secrets.TOKEN)
