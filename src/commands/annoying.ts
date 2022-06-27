29d5010b73d58e9472706d8fe6fa7c0464ef63e0
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-06-27 15:23:01 -0400

    Did a few things. Fixed the start spamming command Added a stop spamming command Integrated into the command handling system.

--- src/commands/annoying.ts
+++ src/commands/annoying.ts
@@ -1,4 +1,5 @@
 import Discord from "discord.js"
+import CommandHandler from "../command-handler"
 
 let isSpamming = false
 
@@ -11,6 +12,28 @@ function startSpamming(context: Discord.Message, commandArguments: string) {
             }
         }
         
-        spam(context.channel as Discord.TextChannel, commandArguments)
+        isSpamming = true
+        
+        let spamMessage = ""
+        if (commandArguments.length < 1) {
+            spamMessage = "i like cute girls"
+        } else {
+            spamMessage = commandArguments
+        }
+        
+        spam(context.channel as Discord.TextChannel, spamMessage)
     }
+}
+
+function stopSpamming(context: Discord.Message, commandArguments: string) {
+    isSpamming = false
+}
+
+function registerCommand(commands: Map<string, CommandHandler>) {
+    commands.set("$$$start_spamming$$$", startSpamming)
+    commands.set("$$$stop_spamming$$$", stopSpamming)
+}
+
+export default {
+    registerCommand
 }
