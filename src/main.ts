bac7f4489e6e70a26a72f40ca071f444d75854fa
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-06-27 15:22:15 -0400

    Finished the command handling system.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import Discord from "discord.js"
 import Secrets from "../data/secrets.json"
+import CommandHandler from "./command-handler"
+
+import AnnoyingCommands from "./commands/annoying"
+
+let commands = new Map<string, CommandHandler>()
+
+AnnoyingCommands.registerCommand(commands)
 
 let client = new Discord.Client({
     intents: [
@@ -10,12 +17,21 @@ let client = new Discord.Client({
 
 client.once("ready", (client) => {
     console.log(`[INFO]: Logged in to Discord as ${client.user.tag}.`)
+    console.log(commands)
     
     client.user.setActivity("OnlyFans", { type: "WATCHING" })
 })
 
 client.on("messageCreate", (message) => {
-    // TODO: Handle commands
+    let commandName = message.content.split(" ")[0]
+    let commandArguments = message.content.substring(commandName.length)
+    
+    let commandHandler = commands.get(commandName)
+    if (commandHandler != undefined) {
+        commandHandler(message, commandArguments)
+    } else {
+        // Do other stuff that are not command related.
+    }
 })
 
 client.login(Secrets.TOKEN)
