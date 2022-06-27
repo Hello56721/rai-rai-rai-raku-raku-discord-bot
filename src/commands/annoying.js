9fe54b1977dae30e43607bfbda37f6eecdb6564f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-06-27 18:58:33 -0400

    Added a start spamming dm command and... Restructured the code a little bit.

--- src/commands/annoying.js
+++ src/commands/annoying.js
@@ -1,31 +1,64 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const utilities_1 = __importDefault(require("../utilities"));
 let isSpamming = false;
-function startSpamming(context, commandArguments) {
-    if (!isSpamming) {
+function registerCommand(commands) {
+    commands.set("$$$start_spamming$$$", (context, commandArguments) => {
+        if (!isSpamming) {
+            function spam(channel, message) {
+                if (isSpamming) {
+                    channel.send(message);
+                    setTimeout(spam, 1500, channel, message);
+                }
+            }
+            isSpamming = true;
+            let spamMessage = "";
+            if (commandArguments.length < 1) {
+                spamMessage = "i like cute girls";
+            }
+            else {
+                spamMessage = commandArguments;
+            }
+            spam(context.channel, spamMessage);
+        }
+    });
+    commands.set("$$$stop_spamming$$$", () => {
+        isSpamming = false;
+    });
+    commands.set("$$$start_spamming_dm$$$", (context, commandArguments) => {
+        var _a;
+        isSpamming = true;
+        let spamTarget = commandArguments.split(" ")[0];
+        let spamMessage = commandArguments.substring(spamTarget.length);
+        if (spamTarget.length == 0) {
+            context.reply("you need to tell me who to spam dm bozo");
+            return;
+        }
+        if (spamMessage.length == 0) {
+            spamMessage = "i like cute girls";
+        }
+        if (utilities_1.default.isMention(spamTarget)) {
+            spamTarget = utilities_1.default.mentionToUserID(spamTarget);
+        }
         function spam(channel, message) {
             if (isSpamming) {
-                channel.send(message);
+                channel.send(message).catch((error) => {
+                    isSpamming = false;
+                    console.log(error);
+                });
                 setTimeout(spam, 1500, channel, message);
             }
         }
-        isSpamming = true;
-        let spamMessage = "";
-        if (commandArguments.length < 1) {
-            spamMessage = "i like cute girls";
-        }
-        else {
-            spamMessage = commandArguments;
-        }
-        spam(context.channel, spamMessage);
-    }
-}
-function stopSpamming(context, commandArguments) {
-    isSpamming = false;
-}
-function registerCommand(commands) {
-    commands.set("$$$start_spamming$$$", startSpamming);
-    commands.set("$$$stop_spamming$$$", stopSpamming);
+        (_a = context.guild) === null || _a === void 0 ? void 0 : _a.members.fetch(spamTarget).then((member) => {
+            context.reply(`spamming ${member.nickname}'s dm lmao :joy_cat:`);
+            spam(member, spamMessage);
+        }).catch(() => {
+            context.reply("something went wrong sorry");
+        });
+    });
 }
 exports.default = {
     registerCommand
--- src/commands/annoying.ts
+++ src/commands/annoying.ts
@@ -1,37 +1,72 @@
 import Discord from "discord.js"
 import CommandHandler from "../command-handler"
+import Utilities from "../utilities"
 
 let isSpamming = false
 
-function startSpamming(context: Discord.Message, commandArguments: string) {
-    if (!isSpamming) {
+function registerCommand(commands: Map<string, CommandHandler>) {
+    commands.set("$$$start_spamming$$$", (context, commandArguments) => {
+        if (!isSpamming) {
+            function spam(channel: Discord.TextChannel, message: string) {
+                if (isSpamming) {
+                    channel.send(message)
+                    setTimeout(spam, 1500, channel, message)
+                }
+            }
+            
+            isSpamming = true
+            
+            let spamMessage = ""
+            if (commandArguments.length < 1) {
+                spamMessage = "i like cute girls"
+            } else {
+                spamMessage = commandArguments
+            }
+            
+            spam(context.channel as Discord.TextChannel, spamMessage)
+        }
+    })
+    
+    commands.set("$$$stop_spamming$$$", () => {
+        isSpamming = false
+    })
+    
+    commands.set("$$$start_spamming_dm$$$", (context, commandArguments) => {
+        isSpamming = true
+        
+        let spamTarget = commandArguments.split(" ")[0];
+        let spamMessage = commandArguments.substring(spamTarget.length)
+        
+        if (spamTarget.length == 0) {
+            context.reply("you need to tell me who to spam dm bozo")
+            return
+        }
+        
+        if (spamMessage.length == 0) {
+            spamMessage = "i like cute girls"
+        }
+        
+        if (Utilities.isMention(spamTarget)) {
+            spamTarget = Utilities.mentionToUserID(spamTarget)
+        }
+        
         function spam(channel: Discord.TextChannel, message: string) {
             if (isSpamming) {
-                channel.send(message)
+                channel.send(message).catch((error) => {
+                    isSpamming = false
+                    console.log(error)
+                })
                 setTimeout(spam, 1500, channel, message)
             }
         }
         
-        isSpamming = true
-        
-        let spamMessage = ""
-        if (commandArguments.length < 1) {
-            spamMessage = "i like cute girls"
-        } else {
-            spamMessage = commandArguments
-        }
-        
-        spam(context.channel as Discord.TextChannel, spamMessage)
-    }
-}
-
-function stopSpamming(context: Discord.Message, commandArguments: string) {
-    isSpamming = false
-}
-
-function registerCommand(commands: Map<string, CommandHandler>) {
-    commands.set("$$$start_spamming$$$", startSpamming)
-    commands.set("$$$stop_spamming$$$", stopSpamming)
+        context.guild?.members.fetch(spamTarget).then((member) => {
+            context.reply(`spamming ${member.nickname}'s dm lmao :joy_cat:`)
+            spam(member as unknown as Discord.TextChannel, spamMessage)
+        }).catch(() => {
+            context.reply("something went wrong sorry")
+        })
+    })
 }
 
 export default {
