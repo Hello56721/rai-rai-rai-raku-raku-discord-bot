29722592772dba5749f16a83bf56aaad00db31cd
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-06-27 18:35:42 -0400

    Added a file for testing stuff.

---
+++ src/tester.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const utilities_1 = __importDefault(require("./utilities"));
+console.log(utilities_1.default.isMention("<@47981237492837>"));
+console.log(utilities_1.default.isMention("hello bozo"));
+console.log(utilities_1.default.isMention("19287123123"));
+//# sourceMappingURL=tester.js.map
---
+++ src/tester.ts
@@ -0,0 +1,5 @@
+import Utilities from "./utilities"
+
+console.log(Utilities.isMention("<@47981237492837>"))
+console.log(Utilities.isMention("hello bozo"))
+console.log(Utilities.isMention("19287123123"))
