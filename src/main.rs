8e9772c7ca935acb80f4ceee12f352b379e154fe
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-01-27 19:37:10 -0500

    Added internal state handling system + ignore self messages.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::id::UserId;
 use serenity::prelude as discord;
 
 use std::process::Command;
+use tokio::sync::Mutex;
 
-struct EventHandler;
+#[derive(Default)]
+struct Bot {
+    id: UserId
+}
+
+struct EventHandler {
+    bot: Mutex<Bot>
+}
 
 #[serenity::async_trait]
 impl discord::EventHandler for EventHandler {
     async fn ready(&self, _: discord::Context, ready: Ready) {
         println!("[INFO]: The bot has logged on as {}", ready.user.name);
+        
+        let mut bot = self.bot.lock().await;
+        bot.id = ready.user.id;
     }
 
     async fn message(&self, context: discord::Context, message: Message) {
         println!("[MESSAGE]: {}", message.content);
+        
+        let bot = self.bot.lock().await;
+        
+        if bot.id == message.author.id {
+            return;
+        }
         
         if message.content.starts_with("I am") {
             if let Err(why) = message.reply_ping(context, "fuk yo").await {
@@ -37,9 +55,11 @@ async fn main() {
     let intents = discord::GatewayIntents::GUILDS
         | discord::GatewayIntents::GUILD_MESSAGES
         | discord::GatewayIntents::MESSAGE_CONTENT;
+        
+    let event_handler = EventHandler { bot: Mutex::new(Bot::default()) };
 
     let mut bot = discord::Client::builder(&token, intents)
-        .event_handler(EventHandler)
+        .event_handler(event_handler)
         .await
         .expect("Failed to create the client. Perhaps the token wasn't valid?");
 
