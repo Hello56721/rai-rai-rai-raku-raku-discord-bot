579a87b492f8e83f518a6b1bc3b9197f2739b893
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-01-28 19:57:07 -0500

    Added safeguard for the restart command + formatted document.

--- src/commands.rs
+++ src/commands.rs
@@ -5,19 +5,29 @@ use serenity::model::application::interaction::InteractionResponseType;
 
 use std::process::Command;
 
+use super::OWNER_ID;
+
 pub async fn restart(context: Context, command: ApplicationCommandInteraction) {
-    let (should_shut_down, response_content) =
-        if let Err(why) = Command::new("cargo").arg("run").spawn() {
-            (false, format!("failed to restart bot bc of {:?}", why))
-        } else {
-            (true, "restarting bot".to_string())
-        };
+    let (should_shut_down, response_content) = if command.user.id != OWNER_ID {
+        (
+            false,
+            "sorry but i dont take orders from idiots like u".to_string(),
+        )
+    } else if let Err(why) = Command::new("cargo").arg("run").spawn() {
+        (false, format!("failed to restart bot bc of {:?}", why))
+    } else {
+        (true, "restarting bot".to_string())
+    };
 
     command
         .create_interaction_response(context, |response| {
             response
                 .interaction_response_data(|message| {
-                    message.content(response_content).ephemeral(true)
+                    if command.user.id == OWNER_ID {
+                        message.content(response_content).ephemeral(true)
+                    } else {
+                        message.content(response_content).ephemeral(false) // for completeness
+                    }
                 })
                 .kind(InteractionResponseType::ChannelMessageWithSource)
         })
@@ -30,21 +40,35 @@ pub async fn restart(context: Context, command: ApplicationCommandInteraction) {
 }
 
 pub async fn dm(context: Context, command: ApplicationCommandInteraction) {
-    let user = command.data.options.get(0).unwrap().resolved.as_ref().unwrap();
-    let message = command.data.options.get(1).unwrap().resolved.as_ref().unwrap();
+    let user = command
+        .data
+        .options
+        .get(0)
+        .unwrap()
+        .resolved
+        .as_ref()
+        .unwrap();
+    let message = command
+        .data
+        .options
+        .get(1)
+        .unwrap()
+        .resolved
+        .as_ref()
+        .unwrap();
 
     if let CommandDataOptionValue::User(user, _member) = user {
         if let CommandDataOptionValue::String(message) = message {
             let response = async {
                 let dm_channel = user.create_dm_channel(context.clone()).await;
-                
+
                 if let Err(_) = dm_channel {
                     format!("cant dm {} :pouting_cat:", user.name)
                 } else if let Ok(dm_channel) = dm_channel {
                     let result = dm_channel
                         .send_message(context.clone(), |msg| msg.content(message))
                         .await;
-                    
+
                     if let Err(_) = result {
                         format!("cant dm {} :pouting_cat:", user.name)
                     } else {
@@ -56,12 +80,13 @@ pub async fn dm(context: Context, command: ApplicationCommandInteraction) {
             }
             .await;
 
-            let result = command.create_interaction_response(context, |response_data| {
-                response_data.interaction_response_data(|response_data| {
-                    response_data.content(response)
+            let result = command
+                .create_interaction_response(context, |response_data| {
+                    response_data
+                        .interaction_response_data(|response_data| response_data.content(response))
                 })
-            }).await;
-            
+                .await;
+
             if let Err(error) = result {
                 eprintln!("[ERROR]: Failed to respond to DM command: {:?}", error);
             }
