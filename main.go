7d3814cd5535b8cbb13e21270529c762061623a5
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-14 14:04:12 -0400

    Created a basic Discord bot.

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    token, error := ioutil.ReadFile("user/token.txt")
+    if error != nil {
+        fmt.Fprintln(os.Stderr, "[ERROR]: Failed to read the token file. Maybe it doesn't exist?")
+        return
+    }
+
+    discord, error := discordgo.New("Bot " + strings.TrimSpace(string(token)))
+    if error != nil {
+        fmt.Fprintln(os.Stderr, "[ERROR]: Failed to login to Discord. Error:", error)
+        return
+    }
+
+    error = discord.Open()
+    if error != nil {
+        fmt.Fprintln(os.Stderr, "[ERROR]: Failed to open a connection to Discord. Error", error)
+        return
+    }
+
+    defer discord.Close()
+
+    signalChannel := make(chan os.Signal, 1)
+    signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+    <-signalChannel
 }
